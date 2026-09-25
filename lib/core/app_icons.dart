92f12344e6520b8ad9e347ff001a20a6cf08a0c8
sheraz-ali <soomrosheraz054@gmail.com>
2026-09-25 23:55:33 +0500

    Herbs and Spices App - Assets set

---
+++ lib/core/app_icons.dart
@@ -0,0 +1,27 @@
+class AppIcons {
+  static const _path = 'assets/icons/';
+
+  static const icAdd = '${_path}ic_add.svg';
+  static const icArrowNext = '${_path}ic_arrow_next.svg';
+  static const icBack = '${_path}ic_back.svg';
+  static const icBadgeOff = '${_path}ic_badge_off.svg';
+  static const icCart = '${_path}ic_cart.svg';
+  static const icCheckoutBag = '${_path}ic_checkout_bag.svg';
+  static const icChevronDown = '${_path}ic_chevron_down.svg';
+  static const icCinnamonIllustration = '${_path}ic_cinnamon_illustration.svg';
+  static const icDetailBlob = '${_path}ic_detail_blob.svg';
+  static const icDetailBlobLeft = '${_path}ic_detail_blob_left.svg';
+  static const icDetailBlobRight = '${_path}ic_detail_blob_right.svg';
+  static const icDetailPagination = '${_path}ic_detail_pagination.svg';
+  static const icDiscountRibbon = '${_path}ic_discount_ribbon.svg';
+  static const icDrawer = '${_path}ic_drawer.svg';
+  static const icFavorite = '${_path}ic_favorite.svg';
+  static const icHeaderWave = '${_path}ic_header_wave.svg';
+  static const icHome = '${_path}ic_home.svg';
+  static const icOnboardingDots = '${_path}ic_onboarding_dots.svg';
+  static const icOnboardingNext = '${_path}ic_onboarding_next.svg';
+  static const icProfile = '${_path}ic_profile.svg';
+  static const icReels = '${_path}ic_reels.svg';
+  static const icSearch = '${_path}ic_search.svg';
+  static const icWelcomeLogo = '${_path}welcome_logo.svg';
+}
--- lib/core/asset_res.dart
+++ lib/core/asset_res.dart
@@ -1,9 +1,11 @@
 class AssetRes {
-  static const _iconsPath = 'assets/icons/';
   static const _imagesPath = 'assets/images/';
 
-  static const icArrowNext = '${_iconsPath}ic_arrow_next.svg';
-
-
-  static const welcomePageImg = '${_imagesPath}welcome_img.png';
+  static const welcomeImg = '${_imagesPath}welcome_img.png';
+  static const imgTexture = '${_imagesPath}img_texture.png';
+  static const imgChillies = '${_imagesPath}img_chillies.png';
+  static const imgStarAnise = '${_imagesPath}img_star_anise.png';
+  static const imgCloves = '${_imagesPath}img_cloves.png';
+  static const imgCardamom = '${_imagesPath}img_cardamom.png';
+  static const imgCinnamon = '${_imagesPath}img_cinnamon.png';
 }
